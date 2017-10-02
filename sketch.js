034d3d70dd081014bac8c2f65fead403de5baf68
Hoyiki <zh603@nyu.edu>
2017-10-02 22:58:33 +0400

    eye

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
+var iPosi;
+var right;
 function setup() {
-  createCanvas(displayHeight, displayWidth);
+  createCanvas(displayWidth, displayHeight);
   background(0);
   noStroke();
+  iPosi = width*0.5;
+  right = true;
 }
 
 function draw() {
@@ -9,5 +13,17 @@ function draw() {
   arc(width/2, height*3.5/4, width/2*1.5, height*1.5, PI+QUARTER_PI, TWO_PI-QUARTER_PI, CHORD);
   arc(width/2, height*(-1/15), width/2*1.5, height*1.5, TWO_PI+QUARTER_PI, PI-QUARTER_PI, CHORD);
   fill(0);
-  ellipse(width/2, height*0.4, width*0.22, height/3);
+  ellipse(iPosi, height*0.4, width*0.22, height/3);
+  if (right){
+      iPosi = iPosi + width * 0.001;
+  }
+  else{
+      iPosi = iPosi - width * 0.001;
+  }
+  if (iPosi>=width*0.6){
+      right = false;
+  }
+  if (iPosi<=width*0.4){
+      right = true;
+  }
 }
