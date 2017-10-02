81084c18996f7b94f6f53c8fb0ff33f66976333e
Hoyiki <zh603@nyu.edu>
2017-10-02 22:23:56 +0400

    p5

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+  background(0);
+  noStroke();
+}
+
+function draw() {
+  fill(255);
+  arc(width/2, height*3.5/4, width/2*1.5, height*1.5, PI+QUARTER_PI, TWO_PI-QUARTER_PI, CHORD);
+  arc(width/2, height*(-1/15), width/2*1.5, height*1.5, TWO_PI+QUARTER_PI, PI-QUARTER_PI, CHORD);
+  fill(0);
+  ellipse(width/2, height*0.4, width*0.22, height/3);
+}
